7fd01511366ec80c0c8c564b7b4e5aed28aca27b
Yash Anand <152001511+yashanandd@users.noreply.github.com>
2026-07-12 01:49:08 +0530

    Time: 0 ms (100%), Space: 63.6 MB (10.76%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -3,15 +3,14 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int st = 0;
         int end = arr.size()-1;
-        while (st<=end){
+        while (st<end){
             int mid = st + (end - st)/2;
-            if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]) return mid;
-            if(arr[mid]<arr[mid-1]){
-                end = mid;
+            if(arr[mid]<arr[mid+1]){
+                st = mid+1;
             }else{
-                st = mid;
+                end = mid;
             }
         }
-        return -1;
+        return st;
     }
 };
